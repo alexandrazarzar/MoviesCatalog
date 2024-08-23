241b57717fd5c9902a3945913c5d4b12753df8f8
Alexandra Zarzar <alexandravzarzar@gmail.com>
2024-08-22 21:35:23 -0300

    Initial Commit

---
+++ MoviesCatalog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoviesCatalog
+//
+//  Created by avz on 22/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoviesCatalog/MoviesCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesCatalogApp.swift
+//  MoviesCatalog
+//
+//  Created by avz on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
