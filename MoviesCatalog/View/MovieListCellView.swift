9da5c2b130291e41017460ed0e4bc5f23ad79e96
Alexandra Zarzar <alexandravzarzar@gmail.com>
2024-08-23 21:10:43 -0300

    Creates struct for movie poster

--- MoviesCatalog/View/MovieListCellView.swift
+++ MoviesCatalog/View/MovieListCellView.swift
@@ -11,18 +11,30 @@ struct MovieListCellView: View {
     @ObservedObject var viewModel: MovieListCellViewModel
     
     var body: some View {
-        HStack {
-            moviePoster
-            VStack(alignment: .leading) {
-                Text(viewModel.title)
-                    .font(.title3)
-                    .bold()
-                HStack {
-                    ratingView
-                    Text(viewModel.releaseDate)
+        ZStack(alignment: .leading) {
+            cardWithShadow
+            HStack {
+                MoviePosterView(imageURL: viewModel.posterURL, relativeFrameSize: 0.2)
+                VStack(alignment: .leading) {
+                    Text(viewModel.title)
+                        .font(.title3)
+                        .bold()
+                    HStack {
+                        ratingView
+                        Text(viewModel.releaseDate)
+                    }
                 }
             }
+            .padding(10)
         }
+        .background()
+        .padding(.horizontal)
+    }
+    
+    var cardWithShadow: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .fill(Color.white)
+            .shadow(color: .gray, radius: 3, x: 0, y: 2)
     }
     
     var ratingView: some View {
@@ -38,28 +50,9 @@ struct MovieListCellView: View {
             )
             .frame(maxWidth: 60, maxHeight: 25)
     }
-    
-    var moviePoster: some View {
-        Group {
-            AsyncImage(url: viewModel.posterURL) { phase in
-                if let image = phase.image {
-                    image
-                        .resizable()
-                } else if phase.error != nil {
-                    Image(systemName: "exclamationmark.octagon.fill")
-                        .resizable()
-                } else {
-                    ProgressView()
-                }
-            }
-        }
-        .scaledToFit()
-        .containerRelativeFrame(.horizontal) { size, axis in
-            size * 0.2
-        }
-    }
 }
 
 #Preview {
     MovieListCellView(viewModel: MovieListCellViewModel(movie: Movie(id: 1, title: "Title", voteAverage: 9.78676, runtime: nil, releaseDate: "2008-08-08", genres: nil, posterPath: "1E5baAaEse26fej7uHcjOgEE2t2.jpg")))
+        .padding(.vertical, 300)
 }
